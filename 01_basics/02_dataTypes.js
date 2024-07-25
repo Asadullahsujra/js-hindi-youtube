35ca0c56590e832155090bbe53918fdd8e70bda1
Asadullahsujra <148256399+Asadullahsujra@users.noreply.github.com>
2024-07-25 14:31:49 +0000

    Update

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -4,3 +4,17 @@
 console.log(3+3);
 
 console.log("Asad"); 
+
+
+let name = "Asad"
+let age = 27
+let isLoggedIn = false
+
+
+
+// number => 2 to power 53
+// bigint =>
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined = 
